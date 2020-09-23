b4dba4fe6fb0687c97e70556eeb21e4af7de1944
nadiariz <nadia_majeed27@hotmail.com>
2020-09-23 16:52:15 -0500

    please check code, nooutput

---
+++ Exercises/lesson19/index.js
@@ -0,0 +1,12 @@
+'use strict';
+const BIRTHMONTH = '13-03-2008';
+const FIRSTNAME = 'Anser';
+const LASTNAME = 'Khan';
+let city, age, nextCity;
+city = "Karachi";
+age = "12";
+nextCity = city;
+console.log('age:12', age); // age
+console.log('city:Karachi', city);//city name
+console.log('nextCity:city',nextCity); // next city must use city name
+alert(`${FIRSTNAME}${LASTNAME}`);
