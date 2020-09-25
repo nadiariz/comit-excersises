73bb3fa32a8d8c5589cc70058bb2cd636dd40e99
nadiariz <nadia_majeed27@hotmail.com>
2020-09-25 01:13:37 -0500

    ex21,result can obtain in two ways?

---
+++ Exercises/lesson21/index.js
@@ -0,0 +1,8 @@
+"use strict";
+alert("please enter two numbers to compare!");
+let firstNumber = prompt("eneter First Number");
+let secondNumber = prompt("enter Second Number");
+let result =(firstNumber>secondNumber);
+alert(result);
+//also let result =confirm(firstNumber>secondNumber)
+
