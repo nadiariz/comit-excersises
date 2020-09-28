bc5dea7b399875e1509e2f939def1353e400652b
nadiariz <nadia_majeed27@hotmail.com>
2020-09-28 14:46:22 -0500

    lesson 22 ex

---
+++ Exercises/lesson22/index.js
@@ -0,0 +1,10 @@
+"use strict";
+alert("please enter two numbers to see if the first is greater than second");
+let firstNumber = Number(prompt("Enter first Number"));
+let secondNumber = Number(prompt("enter Second Number"));
+if (firstNumber>secondNumber){
+    alert("first numer is greatre");
+}
+else {
+    alert("second number is greater");
+}
