9270ad37a02fc8ff85e4130d51a6735d9d06590b
nadiariz <nadia_majeed27@hotmail.com>
2020-10-04 23:41:16 -0500

    part 4 done

--- Exercises/lesson26/lesson26b/index.js
+++ Exercises/lesson26/lesson26b/index.js
@@ -10,9 +10,21 @@ function showUser(){
     if (true){
         console.log("married");
     }
-    else{
+    else {
         console.log("unmarried");
     }
     console.log('name:',name,'age:',age, 'Phone Number:',phoneNumb,'Street Number:',streetNumb,'Postal Code:',Postal, 'Marital Status:',maritalStatus,);
 }
 showUser();
+
+let userName = function (name) {
+    let banner = ""; //remeber to add some value whether it's emty string otherwise it will give undefined
+    for (let equals = 1 ; equals <= name.length+6 ; equals++){
+        banner += "=";
+    }
+    console.log(banner);
+    console.log(`== ${name} ==`); 
+    console.log(banner);
+};
+userName ("Nadia Majeed");
+userName ("Rizwan Khan");
