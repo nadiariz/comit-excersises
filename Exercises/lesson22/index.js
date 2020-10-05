8d8939e9fd4d6d56b30385f32c575618d33d4b75
nadiariz <71297359+nadiariz@users.noreply.github.com>
2020-10-04 22:58:11 -0500

    Update index.js

--- Exercises/lesson22/index.js
+++ Exercises/lesson22/index.js
@@ -3,7 +3,7 @@ alert("please enter two numbers to see if the first is greater than second");
 let firstNumber = Number(prompt("Enter first Number"));
 let secondNumber = Number(prompt("enter Second Number"));
 if (firstNumber>secondNumber){
-    alert("first numer is greatre");
+    alert("first number is greatre");
 }
 else {
     alert("second number is greater");
